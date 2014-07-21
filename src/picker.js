28900b2ab84c7a3bd126053efe7c0162caa9fe44
Rob Harper <rharper@oculusinfo.com>
2014-07-21 15:50:15 -0400

    Fix early return failure to reset context

--- src/picker.js
+++ src/picker.js
@@ -18,12 +18,9 @@ Picker.prototype = {
     lx -= tx;
     ly -= ty;
 
-    var result = node.pick(ctx, x, y, lx, ly);
-    if (result) {
-      return node;
-    }
+    var result = node.pick(ctx, x, y, lx, ly) && node;
 
-    if (node.children) {
+    if (!result && node.children) {
       for (var i=node.children.length-1; i>=0; i--) {
         result = this.pick(node.children[i], x, y, lx, ly);
         if (result) {
