7ec4438931360c725d7a3c57e73225a504f201bf
chris-dickson <chris.r.dickson@gmail.com>
2014-11-06 20:51:39 -0500

    Add circles to a list of basic primitives

---
+++ src/circle.js
@@ -0,0 +1,59 @@
+var _ = require('./util');
+var Node = require('./node');
+
+var NONE = [];
+
+/**
+ * Circl Node
+ *
+ * Properties inherited from Node: visible, x, y, rotation, scaleX, scaleY, opacity
+ *
+ * radius : the radius of the circle
+ * (x,y) : correspond to the center of the circle
+ * fillStyle, strokeStyle, lineWidth:
+ *   as specified in the HTML5 Canvas API
+ * lineDash: an array specifying on/off pixel pattern
+ *   (e.g. [10, 5] = 10 pixels on, 5 pixels off) (not supported in all browsers)
+ * lineDashOffset: a pixel offset to start the dashes (not supported in all browsers)
+ *
+ * Note: picking is always enabled on the entire circle (no stroke-only picking) at
+ * the moment.
+ */
+var Circle = function() {
+  Node.apply(this, arguments);
+};
+
+
+Circle.prototype = _.extend(Circle.prototype, Node.prototype, {
+  draw: function(ctx) {
+    var radius = this.radius || 0;
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
+
+    if (this.fillStyle) {
+	  ctx.fillStyle = this.fillStyle;
+	  ctx.fill();
+    }
+    if (this.strokeStyle) {
+      ctx.strokeStyle = this.strokeStyle;
+      ctx.lineWidth = this.lineWidth || 1;
+      ctx.lineCap = this.lineCap || 'butt';
+      ctx.lineJoin = this.lineJoin || 'miter';
+      ctx.miterLimit = this.miterLimit || 10;
+      ctx.setLineDash(this.lineDash || NONE);
+      ctx.setLineDashOffset(this.lineDashOffset || 0);
+      ctx.stroke();
+    }
+	ctx.closePath();
+  },
+
+  hitTest: function(ctx, x, y, lx, ly) {
+	var dist = Math.sqrt( (this.x - lx)*(this.x - lx) + (this.y - ly)*(this.y - ly))
+    if (dist < this.radius) {
+      return this;
+    }
+  }
+});
+
+
+module.exports = Circle;
